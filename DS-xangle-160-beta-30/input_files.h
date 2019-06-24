348f9399e32873c7a5ec3bf684d8b5c7a609b5c6
Jan Kašpar <jan.kaspar@cern.ch>
2019-06-24 11:43:09 +0200

    Switched to input version7.

--- DS-xangle-160-beta-30/input_files.h
+++ DS-xangle-160-beta-30/input_files.h
@@ -8,17 +8,17 @@ std::vector<std::string> input_files;
 void InitInputFiles()
 {
 	input_files = {
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.0_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.10_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.11_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.1_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.2_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.3_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.4_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.5_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.6_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.7_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.8_re_reco.root",
-		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version6/run_314247.9_re_reco.root"
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.0_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.10_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.11_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.1_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.2_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.3_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.4_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.5_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.6_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.7_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.8_re_reco.root",
+		"root://eostotem.cern.ch//eos/cms/store/group/phys_pps/reconstruction/2018/alignment_run_April/version7/run_314247.9_re_reco.root"
 	};
 }
